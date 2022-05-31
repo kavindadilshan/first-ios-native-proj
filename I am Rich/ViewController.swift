8528d0cbdb55bf848ca0dcecdd1511f28f703e86
kavindadilshan <kavindadilshan97@gmail.com>
2022-05-31 12:27:00 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ceyentra Technologies on 2022-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
