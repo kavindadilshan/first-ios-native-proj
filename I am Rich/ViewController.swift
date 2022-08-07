d1731f9b535ac19dea75ab91ff47ec4a9f71b3ac
kavindadilshan <kavindadilshan97@gmail.com>
2022-08-07 18:51:59 +0530

    face 01

--- I am Rich/ViewController.swift
+++ I am Rich/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+//        diceImageViewOne.alpha = 0.5
+        
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceThree")
     }
+    
+    /**
+     you can use get image literal with #imageLiteral()
+     */
 
-
+    @IBAction func onButtonPress(_ sender: UIButton) {
+        var leftDiceNumber = 1
+        print("left dice number \(leftDiceNumber)")
+//        diceImageViewOne.image = #imageLiteral(resourceName: "DiceTwo")
+//        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageViewOne.image = [#imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceFour")][leftDiceNumber]
+        leftDiceNumber = leftDiceNumber + 1
+    }
 }
 
